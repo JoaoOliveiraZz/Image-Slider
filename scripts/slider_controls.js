2dbe27ba936c284124b148638d1f43ac5f415072
JoaoOliveiraZz <linajoaovictor@gmail.com>
2024-01-13 17:52:28 -0300

    :sparkles: Rendering the navigation controls based on the numbers of images.

--- scripts/slider_controls.js
+++ scripts/slider_controls.js
@@ -1,12 +1,30 @@
-let slider_nav_controls = document.querySelectorAll(".slider_nav_control");
-let slider = document.querySelector(".slider");
+const slider_nav = document.querySelector('.slider_nav')
+const slider = document.querySelector(".slider");
+let sliderImages = [];
+
+window.addEventListener('load', resetToFirstImageOnReload())
 
 slider.addEventListener("scroll", () => {
-  let imageId = window.location.href.split('/').reverse()[0].split('#').reverse()[0]
-
+  let slider_nav_controls = document.querySelectorAll(".slider_nav_control");
+  let imageId = window.location.href.split('/').reverse()[0].split('#')[1]
   slider_nav_controls.forEach(control => {
     control.classList.remove('active')
   });
   document.querySelector(`a[href*='#${imageId}']`).classList.add('active')
 });
+
+function renderControls (){
+    let activeId = window.location.href.split('/').reverse()[0].split('#')[1]
+    const slider_nav_controls = sliderImages.map((item, index) => {
+      return `<a href="#image-${index+1}" class="slider_nav_control ${(`image-${index+1}` === activeId || (sliderImages.length === 1 && index+1 === 1)) ? 'active' : ''}"></a>`
+    }).join('')
+
+    if(sliderImages.length === 1) resetToFirstImageOnReload
+
+    slider_nav.innerHTML = slider_nav_controls
+}
+
+function resetToFirstImageOnReload(){
+  window.location.href = 'index.html#image-1'
+}
 
